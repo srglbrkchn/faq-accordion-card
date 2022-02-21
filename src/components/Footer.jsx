a476b5133a860e37ba6486eff952c8de67678ea4
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-21 22:41:36 +0100

    Initial commit.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div class="attribution">
+            Challenge by
+            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by
+            <a href="https://github.com/srglbrkchn">srglbrkchn</a>.
+        </div>
+    );
+}
+
+export default Footer;
