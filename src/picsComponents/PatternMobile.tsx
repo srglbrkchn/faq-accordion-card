ff4b21cdb0a0d8d94db5e1a6f16589deeb9e08f2
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-03-09 23:46:22 +0100

    Finish tablet design, mobile design. Final commit.

--- src/picsComponents/PatternMobile.tsx
+++ src/picsComponents/PatternMobile.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { SVGProps } from "react";
 
 const SvgPatternMobile = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={240} height={104} xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg className="pattern-mobile" width={240} height={104} xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       d="M116.454 102.048 4.667 34.239c-3.56-2.054-6.61-5.42-3.142-7.627l112.184-66.364c1.682-1.07 4.237-1.773 5.951-.75l117.328 69.904c3.559 2.12 4.198 8.028.652 10.17L131.185 101.94a14.53 14.53 0 0 1-14.731.107z"
       fill="#6862E6"
